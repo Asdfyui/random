2a721eef2e98973a4a392ed8161856f64db0542d
Asdfyui <78831289+Asdfyui@users.noreply.github.com>
2021-02-23 14:48:16 -0800

    Update Main3.java

--- Main3.java
+++ Main3.java
@@ -2,23 +2,19 @@ public class Main3 {
     public static void main(String[] args) {
 
         int[] numbers = new int[10];
-        int sum;
+        int sum, swap, swapp;
         sum = 0;
-
-
         for (int i = 0; i < numbers.length; i++) {
             int random = (int)(Math.random()*100 + 1);
             numbers[i] = random;
-
-            int swap = numbers[2];
-            int swapp = numbers[3];
-            numbers[2]= swapp;
-            numbers[3]= swap;
-            System.out.println(numbers[i]);
             sum = sum + numbers[i];
+            System.out.println(numbers[i]);
         }
-
-
+        swapp = numbers[2];
+        swap = numbers[3];
+        numbers[2] = swap;
+        numbers[3] = swapp;
+        System.out.println(numbers[i]);
         System.out.println("The average value is " + sum/10 );
     }
 }
