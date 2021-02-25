16997c6850feed5ce77f481ad746a726ac59d1c4
Asdfyui <78831289+Asdfyui@users.noreply.github.com>
2021-02-25 14:55:44 -0800

    sleepy1

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+    public static int[] addOn(int[] array, int number) {
+        int[] newarray = new int[array.length + 1];
+        newarray[0] = array[0];
+        newarray[1] = array[1];
+        newarray[2] = number;
+        return newarray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[2];
+        for (int i = 0; i < array.length; i++) {
+            int random = (int) (Math.random() * 100 + 1);
+            array[i] = random;
+        }
+        System.out.println(Arrays.toString(addOn(array, 12)));
+    }
+}
+
+
+
