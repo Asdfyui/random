7c2b99f3568db88663ba4e2e964e5e2a6c771d18
Asdfyui <78831289+Asdfyui@users.noreply.github.com>
2021-02-26 14:05:45 -0800

    sleepy2

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main1 {
     public static int[] remove(int[] array) {
         int[] newarray = new int[array.length - 1 ];
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length - 1; i++){
             newarray[i] = array[i];
         }
         return newarray;
@@ -16,8 +16,9 @@ public class Main1 {
             array[i] = random;
             System.out.println(array[i]);
         }
+
         System.out.println("****");
+        System.out.println("Deleted value is " + array[array.length - 1]);
         System.out.println(Arrays.toString(remove(array)));
     }
 }
-
