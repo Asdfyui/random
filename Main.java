3a6acf99d4539e04e3018bba04e0078b27e4403f
Asdfyui <78831289+Asdfyui@users.noreply.github.com>
2021-02-26 14:51:47 -0800

    SLEEPY4

--- Main.java
+++ Main.java
@@ -1,23 +1,30 @@
 import java.util.Arrays;
 
-public class Main {
-    public static int[] addOn(int[] array, int number) {
-        int[] newarray = new int[array.length + 1];
+public class Main1 {
+    public static int[] addin(int[] array, int number, int number1) {
+        int[] newarray = new int[array.length];
+        newarray[number1] = number;
         for (int i = 0; i < array.length; i++){
             newarray[i] = array[i];
-            newarray[array.length] = number;
+            if (newarray[number1] == number){
+                newarray = new int[array.length + 1];
+
+            }
         }
-            return newarray;
+        newarray[number1] = number;
+
+        return newarray;
     }
 
     public static void main(String[] args) {
-        int[] array = new int[9];
+        int[] array = new int[4];
         for (int i = 0; i < array.length; i++) {
             int random = (int) (Math.random() * 100 + 1);
             array[i] = random;
             System.out.println(array[i]);
         }
+
         System.out.println("****");
-        System.out.println(Arrays.toString(addOn(array, 656)));
+        System.out.println(Arrays.toString(addin(array, 1222, 2)));
     }
 }
