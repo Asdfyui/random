1fd5771f30d39add5c1138e2ef0719aff09cbf7a
Asdfyui <78831289+Asdfyui@users.noreply.github.com>
2021-04-09 15:01:48 -0700

    doubleadd

--- CalcFX/src/sample/Controller.java
+++ CalcFX/src/sample/Controller.java
@@ -1,131 +1,155 @@
 package sample;
 
-import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
 import java.util.ArrayList;
 
 public class Controller {
 
-    public ArrayList <String> numbers = new ArrayList<>();
+    public ArrayList<String> numbers = new ArrayList<>();
+    public ArrayList<Integer> addArray = new ArrayList<>();
+    public ArrayList<Integer> multiplyArray = new ArrayList<>();
+    public ArrayList<Integer> subtractArray = new ArrayList<>();
+
+
     public Label Total;
 
 
-    public void Zero(ActionEvent actionEvent) {
+    public void Zero() {
         addNumber("0");
     }
 
-    public void One(ActionEvent actionEvent) {
+    public void One() {
         addNumber("1");
     }
 
-    public void Two(ActionEvent actionEvent) {
+    public void Two() {
         addNumber("2");
 
     }
 
-    public void Three(ActionEvent actionEvent) {
+    public void Three() {
         addNumber("3");
     }
 
-    public void Four(ActionEvent actionEvent) {
+    public void Four() {
         addNumber("4");
     }
 
-    public void Five(ActionEvent actionEvent) {
+    public void Five() {
         addNumber("5");
     }
 
-    public void Six(ActionEvent actionEvent) {
+    public void Six() {
         addNumber("6");
     }
 
-    public void Seven(ActionEvent actionEvent) {
+    public void Seven() {
         addNumber("7");
 
     }
 
-    public void Eight(ActionEvent actionEvent) {
+    public void Eight() {
         addNumber("8");
 
     }
 
-    public void Nine(ActionEvent actionEvent) {
+    public void Nine() {
         addNumber("9");
     }
 
-    public void Add(ActionEvent actionEvent) {
-       clickOperator();
-       numbers.add("+");
-       }
+    public void Add() {
+        clickOperator();
+        numbers.add("+");
+    }
 
-    public void Subtract(ActionEvent actionEvent) {
+    public void Subtract() {
         clickOperator();
         numbers.add("-");
     }
 
-    public void Multiply(ActionEvent actionEvent) {
+    public void Multiply() {
         clickOperator();
         numbers.add("*");
     }
 
-    public void Equals(ActionEvent actionEvent) {
+    public void Equals() {
         clickOperator();
         int count2 = 1;
         int count = 0;
+        int addedCount = 0;
+        int subtractedCount = 0;
+        int multipliedCount = 0;
         int added = 0;
         int subtracted = 0;
         int multiplied = 0;
-        for (int i = 1; i <= numbers.size(); ++i){
-            if (count2 < numbers.size()){
-                if (numbers.get(count2).equals("+"));{
+        int addedA = 0;
+        int subtractedA = 0;
+        int multipliedA = 0;
+        for (int i = 1; i <= numbers.size(); ++i) {
+            if (count2 < numbers.size()) {
+                if (numbers.get(count2).equals("+"))
+                {
 
-                    if(count + 2 <= numbers.size()) {
-                        if (count == 0){
-                            added = Integer.parseInt(numbers.get(count)) + Integer.parseInt(numbers.get(count + 2));
+                    if (count + 2 <= numbers.size()) {
+                        if (addedCount == 0) {
+                            addArray.add(Integer.parseInt(numbers.get(count)) + Integer.parseInt(numbers.get(count + 2)));
+                            added = addArray.get(addedA);
+                            addedCount++;
                         }
-                        if (count != 0){
-                            added = added + Integer.parseInt(numbers.get(count)) + Integer.parseInt(numbers.get(count + 2));
+                        if (addedCount != 0) {
+                            addArray.add(addArray.get(addedA) + Integer.parseInt(numbers.get(count + 2)));
+                            addedA++;
+                            added = addArray.get(addedA);
+
+
                         }
                     }
                 }
-                if (numbers.get(count2).equals("-"));{
+                if (numbers.get(count2).equals("-"))
+                {
+                    if (count + 2 <= numbers.size()) {
+                        if (subtractedCount == 0) {
+                            subtractArray.add(Integer.parseInt(numbers.get(count)) - Integer.parseInt(numbers.get(count + 2)));
+                            subtracted = subtractArray.get(subtractedA);
+                            subtractedCount++;
+                        }
+                        if (subtractedCount != 0) {
+                            subtractArray.add(addArray.get(subtractedA) - Integer.parseInt(numbers.get(count + 2)));
+                            subtractedA++;
+                            subtracted = subtractArray.get(addedA);
 
-                    if (count == 0){
-                        added = Integer.parseInt(numbers.get(count)) + Integer.parseInt(numbers.get(count + 2));
-                    }
-                    if (count != 0){
-                        added = added + Integer.parseInt(numbers.get(count)) + Integer.parseInt(numbers.get(count + 2));
+
+                        }
                     }
                 }
-                if (numbers.get(count2).equals("*"));{
-                    if (count == 0){
+                if (numbers.get(count2).equals("*"))
+                {
+                    if (count == 0) {
                         multiplied = Integer.parseInt(numbers.get(count)) * Integer.parseInt(numbers.get(count + 2));
                     }
-                    if (count != 0){
-                            multiplied = multiplied * Integer.parseInt(numbers.get(count)) * Integer.parseInt(numbers.get(count + 2));
+                    if (count != 0) {
+                        multiplied = multiplied * Integer.parseInt(numbers.get(count)) * Integer.parseInt(numbers.get(count + 2));
                     }
-
                 }
             }
 
-            count2++;
-            count2++;
-            count++;
-            count++;
+        count2++;
+        count2++;
+        count++;
+        count++;
 
 
-        }
-        System.out.println(added);
-
+    }
+        Total.setText(String.valueOf(added + subtracted + multiplied));
 
 
     }
 
-    public void Clear(ActionEvent actionEvent) {
+
+    public void Clear() {
         numbers.clear();
+        addArray.clear();
+        subtractArray.clear();
         Total.setText("0");
     }
 
